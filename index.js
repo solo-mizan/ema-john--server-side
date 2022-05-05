3eff28abe9e46ca33cf744ebe49814453d47f342
solo-mizan <mizanbd.eee@gmail.com>
2022-05-06 00:05:23 +0600

    async function run

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
@@ -12,14 +13,31 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.udusb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('database connected');
-    client.close();
-});
 
 
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        app.get('/productCount', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const count = await cursor.count();
+            res.send({ count });
+        })
+    }
+    finally { }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('John is running for Ema');
